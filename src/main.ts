c3147104ff6cc3822eb90908b581ed4c2fe9f2ba
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2026-02-21 09:42:40 +0100

    initail

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './style.css' // ← this was the missing piece
 
 const app = createApp(App)
 
